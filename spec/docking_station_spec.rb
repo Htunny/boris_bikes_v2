73577de4e915a071fa96e77bd6e2c449ff92cece
Andrew <andy_htun@hotmail.com>
2016-01-02 18:10:15 +0000

    doubles added

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,6 +3,7 @@ require 'docking_station'
 describe DockingStation do
 
   subject(:docking_station) { described_class.new }
+  let(:bike) { double(:bike, :working? => true) }
 
 describe '#initializing docking station' do
   it 'allows user to set capacity when initializing docking station ' do
@@ -16,7 +17,6 @@ end
 
 describe '#releasing a bike' do
   it 'responds to release_bike' do
-    bike = Bike.new
     subject.dock(bike)
     expect(docking_station).to respond_to(:release_bike)
   end
@@ -26,15 +26,13 @@ describe '#releasing a bike' do
   end
 
   it 'expects bike to be working' do
-    bike = Bike.new
     docking_station.dock(bike)
     docking_station.release_bike
     expect(bike).to be_working
   end
 
   it 'raises error if bike is broken' do
-    bike = Bike.new
-    bike.report_broken
+    allow(bike).to receive(:working?).and_return false
     docking_station.dock(bike)
     expect{docking_station.release_bike}.to raise_error("Bike is broken")
   end
@@ -42,20 +40,17 @@ end
 
 describe '#docking a bike' do
   it 'raises an error when full' do
-     bike = Bike.new
      docking_station.capacity.times do subject.dock(bike) end
      expect { docking_station.dock(bike) }.to raise_error 'Docking station has reached capacity'
   end
 
   it 'raises error when full with new default capacity' do
     docking_station = DockingStation.new(50)
-    bike = Bike.new
     50.times { docking_station.dock(bike) }
     expect{ docking_station.dock(bike) }.to raise_error 'Docking station has reached capacity'
   end
 
   it 'docks a bike' do
-    bike = Bike.new
     expect(docking_station.dock(bike)).to eq [bike]
   end
 end
